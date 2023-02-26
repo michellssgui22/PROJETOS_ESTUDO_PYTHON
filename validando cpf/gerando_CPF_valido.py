640b0c8ff2717b7a5c7da0d34fb100326642608b
michellssgui <michellss.gui@gmail.com>
2023-02-25 22:05:09 -0300

    Criador de CPF

--- validando cpf/gerando_CPF_valido.py
+++ validando cpf/gerando_CPF_valido.py
@@ -1,22 +1,26 @@
 import re
 
 def validacpf():
-    cpf = input("Informe o CPF para validação: ")
+    cpf = input("Digite 9 números aleatórios: ")
 
     cpf = re.sub(
         r'[^0-9]',
         '',
         str(cpf)
     )
-    
-    cpf_clear = cpf[:9]
 
+    cpf_clear = cpf
     #Verificando se não se trata de caracteres repetidos
     caracter_1 = cpf_clear[0]
     teste = len(cpf_clear) * caracter_1
     if cpf_clear == teste:
         print()
-        print('CPF inválido')
+        print('Caracteres repedidos não são válidos!')
+        print()
+        return validacpf()
+    elif len(cpf_clear) > 9 or len(cpf_clear) < 9:
+        print()
+        print('É necessário que sejam informado 9 números exatamente!')
         print()
         return validacpf()
     else:
@@ -26,7 +30,7 @@ def validacpf():
             n.append(int(i) * c)
             c = c -1
 
-        digito_1 = (sum(n) * 10) % 11
+        digito_1 = int((sum(n) * 10) % 11)
         digito_1 = digito_1 if digito_1 <=9 else 0
 
         cpf_clear_2 = cpf_clear + str(digito_1)
@@ -37,16 +41,10 @@ def validacpf():
         for i in cpf_clear_2 :
             n.append(int(i) * c)
             c = c -1
-        digito_2 = (sum(n) * 10) % 11
+        digito_2 = int((sum(n) * 10) % 11)
         digito_2 = digito_2 if digito_2 <=9 else 0
 
-        valida_cpf = cpf_clear + str(digito_1) + str(digito_2)
-
-        if cpf == valida_cpf:
-            print()
-            print(f"O CPF informado, {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{str(digito_1)}{str(digito_2)}, é válido!")
-        else:
-            print()
-            print(" >>>> CPF inválido! <<<<")
+        print()
+        print(f"O CPF criado é:  {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{str(digito_1)}{str(digito_2)}")
 
 validacpf()            
